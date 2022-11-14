f05d09d4b96f13ae00d771e76f00092721637614
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2022-11-14 22:16:47 +0900

    fix index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,12 @@
 <template>
   <section class="container">
     <div>
-      {{ users[0] }}
+      <!-- {{ users[0].id }}, {{ users[0].name }} -->
+      <ul>
+        <li v-for="user in users" :key="user.id">
+          {{ user.id }}, {{ user.name }}, {{ user.company.name }}
+        </li>
+      </ul>
     </div>
   </section>
 </template>
@@ -11,11 +16,15 @@ const axios = require('axios')
 let url = 'https://jsonplaceholder.typicode.com/users'
 
 export default {
-  asyncData({ params }){
+  asyncData({ params, error }) {
     return axios.get(url)
       .then((res) => {
         return { users: res.data }
       })
+      .catch((e => {
+        // console.log(e.response.status)
+        error({ users: e.response.status, message: 'Error!!!!!' })
+      }))
   }
 }
 </script>
